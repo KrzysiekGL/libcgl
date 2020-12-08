d6f39879a2e3616081a091b7bc772ef90a885755
KrzysiekGL <webmaster@unexpectd.com>
2020-12-08 18:27:10 +0100

    Add comments to the header of Camera class and vaugly descreibe how it works

--- src/Camera.h
+++ src/Camera.h
@@ -1,3 +1,10 @@
+/*
+ * Camera class was designed to simulate a camera behavior inside a OpenGL context
+ * All it does is:
+ * - determine UP, FRONT, and RIGHT of a camera
+ * - get the VIEW MATRIX for transformation pipeline
+ * - handle keyboard and mouse input to modify camera parameters accordingly
+ */
 #ifndef CAMERAH
 #define CAMERA
 
@@ -9,14 +16,40 @@
 namespace CGL {
 	class Camera {
 	public:
+		/* Create a camera and assign default parameter's values */
 		Camera();
 		~Camera();
 
+		/* Get the VIEW MATRIX from a camera */
 		glm::mat4 GetViewMatrix();
+		/*
+		 * Handle keyboard input
+		 * Currently set to:
+		 * W - move camera front
+		 * S - move camera backwards
+		 * A - strafe camera left
+		 * D - strafe camera right
+		 * L_CRTL - move camera up
+		 * SPACE - move camera down
+		 */
 		void KeyInputProcess(GLFWwindow* window, float deltaTime);
+		/*
+		 * Handel mouse input
+		 * Camera is using pitch and yaw of Euler angles (roll is not handled)
+		 * Currently set to:
+		 * M_VERT_UP - pitch camera up
+		 * M_VERT_DOWN - pitch camera down
+		 * M_HORIZ_LEFT - yaw camera left
+		 * M_HORIZ_RIGHT - yaw camera right
+		 */
 		void MouseInputProcess(GLFWwindow* window);
 
 	private:
+		/*
+		 * Update camera's parameters (fields)
+		 * after they were changed, so the VIEW MATRIX
+		 * will be changed as well
+		 */
 		void updateCameraVectors();
 
 		float
